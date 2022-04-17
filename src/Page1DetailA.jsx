f46d39e639cd19b227732ac2b4c3cc466139e8b5
tarian <70381096+4masa6@users.noreply.github.com>
2022-04-17 19:33:30 +0900

    stateを用いてページ遷移でデータを渡す

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,4 +1,10 @@
+import { useLocation } from "react-router-dom";
+
 export const Page1DetailA = () => {
+  // ルーティングでpathと一緒にstateを渡すと、useLocationから取得できる
+  const { state } = useLocation();
+  console.log(state);
+
   return (
     <div>
       <h1>Page1DetailAページです</h1>
