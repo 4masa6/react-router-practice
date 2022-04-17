84d7eb3726069ac63f240828d567d99e8e94b8bd
tarian <70381096+4masa6@users.noreply.github.com>
2022-04-17 18:36:00 +0900

    ReactRouterの基本的な使い方

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+
 import { Home } from "./Home";
 import { Page1 } from "./Page1";
 import { Page2 } from "./Page2";
@@ -5,10 +7,31 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <Home />
-      <Page1 />
-      <Page2 />
-    </div>
+    // BrowserRouterで囲った中でルーティングが有効になる
+    // 一番親コンポーネントのApp.jsで囲っておく
+    <BrowserRouter>
+      <div className="App">
+        {/* link to で 指定したパスへのリンクを作成 */}
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/page1">Page1</Link>
+        <br />
+        <Link to="/page2">Page2</Link>
+      </div>
+      {/* Switchでパスに応じて表示するコンポーネントを切り分ける */}
+      <Switch>
+        <Route exact path="/">
+          {" "}
+          {/* exact: 完全一致したパスを対象にする */}
+          <Home />
+        </Route>
+        <Route path="/page1">
+          <Page1 />
+        </Route>
+        <Route path="/page2">
+          <Page2 />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
